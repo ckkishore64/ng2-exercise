c254f4bdef8c3f390dbdeccf0500681d17960796
ckkishore64 <ckkishore64@gmail.com>
2017-06-26 00:12:22 +0530

    Add saveAsPdf functionality in details page

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,19 +1,38 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, Inject } from '@angular/core';
 import { Router } from '@angular/router';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 import { Product } from '../product';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
+  providers: [{ provide: 'Window', useValue: window }]
 })
 export class ProductDetailsComponent {
   @Input() product: Product;
   @Output() goToDetail = new EventEmitter();
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    @Inject('Window') private window: Window
+  ) { }
 
   goBack(): void {
     this.goToDetail.emit();
+  }
+
+  saveAsPdf(): void {
+    this.getCanvas().then(canvas => {
+      const img = canvas.toDataURL("image/png");
+      const doc = new jsPDF({ unit: 'px', format: 'a4' });
+      doc.addImage(img, 'JPEG', 20, 20);
+      doc.save(`${this.product.category}_${this.product.title}.pdf`);
+    })
+  }
+
+  private getCanvas(): any {
+    return html2canvas(document.getElementById('product-pdf'));
   }
 }
